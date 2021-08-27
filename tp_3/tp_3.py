c755bb624a03830792dbbe3bf012275c9ea4f36a
jemf17 <40602289+jemf17@users.noreply.github.com>
2021-08-27 19:19:55 -0300

    hice el punto 2 , 5 y cree el generador automatico de isbn

--- tp_3/tp_3.py
+++ tp_3/tp_3.py
@@ -1,3 +1,4 @@
+import random 
 class Libro:
     def __init__(self,isbn,title,gender,idiom,price):
         self.isbn = isbn
@@ -9,6 +10,9 @@ library = []
 def register_book(isbn,title,gender,idiom,price):
     book = Libro(isbn,title,gender,idiom,price)
     library.append(book)
+
+
+
 def isbn_condition_1(isbn):
     cont = 0
     for i in isbn:
@@ -26,7 +30,10 @@ def isbn_condition_2(isbn):
         if i == '-' and isbn[cont-1] == '-':
             rta = False
         cont += 1
-    return rta
+    if isbn[0] != '-' and isbn[-1] != '-':
+        return rta
+    else:
+        return False
 
 def isbn_condition_3(isbn):
         expo = 10
@@ -40,20 +47,64 @@ def isbn_condition_3(isbn):
         else :
             return False
 
-def intput_book(n):
+def create_isbn():
+    it = ['0','1','2','3','4','5','6','7','8','9','-']
+    a = False
+    isbn = ''
+    while a == False:
+        for i in range(14):
+            isbn += random.choice(it)
+        if isbn_condition_1(isbn) and isbn_condition_2(isbn) and isbn_condition_3(isbn):
+            return isbn
+        else:
+            isbn = ''
+def intput_book(n, auto):
     while n != 0:
-        isbn = input('isbn: ')
+        if auto:
+            isbn = create_isbn()
+        else:
+            isbn = input('isbn: ')
         title = input('title: ')
         gender = input('gender: ')
         idiom = input('idiom: ')
-        price = input('price: ')
+        price = int(input('price: '))
         if isbn_condition_1(isbn) and isbn_condition_2(isbn) and isbn_condition_3(isbn):
             register_book(isbn,title,gender,idiom,price)
             n -= 1
         else:
             print('el isbn no es valido')
-
 n = int(input('numero de libros a agregar: '))
-intput_book(n)
- 
-print(library[0].title, library[0].isbn)
+intput_book(n, True)
+
+
+
+def order_gender(v):
+    n = len(v)
+    for i in range(n-1):
+        ordenado = True
+        for j in range(n-i-1):
+            if v[j].title[0].lower() > v[j+1].title[0].lower():
+                ordenado = False
+                v[j].title, v[j+1].title = v[j+1].title, v[j].title
+            if ordenado:
+                break
+order_gender(library)
+def show(list):
+    for i in range(len(list)):
+        print('ISBN: {}, Title: {}, Idiom:{}'.format(list[i].isbn,list[i].title, list[i].idiom))
+show(library)
+
+def search_isbn(isbn, vec):
+    price = lambda p: p+(p/10)
+    pos = 0
+    for i in range(len(vec)):
+        if vec[i].isbn == isbn:
+            pos = i
+            break
+    if vec[pos].isbn == isbn:       
+        return 'ISBN:{}, Title: {}, Gender:{}, Idiom:{}, Price:{}'.format(vec[pos].isbn,vec[pos].title,vec[pos].gender,vec[pos].idiom,price(vec[pos].price))
+    else:
+        return 'no se encontro :('
+i = input('ingrese isbn: ')
+
+print(search_isbn(i, library))
